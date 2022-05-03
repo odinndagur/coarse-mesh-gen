417d1a45ddec26d84245df39c87d84869a904087
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2022-05-03 12:54:29 +0000

    lol

--- CoarseMeshGen.cs
+++ CoarseMeshGen.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter),typeof(MeshRenderer), typeof(MeshCollider))]
 public class CoarseMeshGen : MonoBehaviour
 {
-
     float[] bigArr;
     int height = 2499;
     int width = 2500;
@@ -41,111 +40,25 @@ public class CoarseMeshGen : MonoBehaviour
         updateOnGPU();
     }
     void Update(){
-        // t = GameObject.Find("LerpController").GetComponent<LerpController>().t;
-        // GenerateMesh();
-        // UpdateMesh();
         if(t != lastT){
             updateOnGPU();
             Debug.Log("gpu");
             lastT = t;
         }
                 t = (Mathf.Sin(Time.time * morphSpeed) + 1)/2;
-
     }
 
 
-
-    //  #if UNITY_EDITOR
-    // instead of @script ExecuteInEditMode()
     [ContextMenu("Generate mesh")]
     void GenerateMesh() 
     {
-        // loadData();
-        // loadDataWithSkips();
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         CreateShape();
         UpdateMesh();
     }
-    [ContextMenu("Load Data")]
-    void loadDataWorks(){
-        bigArr = new float[width*height];
-        try
-        {
-            using (var fileStream = System.IO.File.OpenRead("/Users/odinndagur/Code/Github/binary_writer_py/test_heights2.dat"))
-            using (var reader = new System.IO.BinaryReader(fileStream))
-            {
-                for(int i = 0; i < width * height; i++){
-                    bigArr[i] = reader.ReadSingle();
-                }
-            }
-        }
-        catch(System.Exception e){ // handle errors here.
-            Debug.Log(e);
-        }
-    }
-
-    void loadData(){
-        width = 25000;
-        height = 24999;
-        bigArr = new float[width*height];
-        try
-        {
-            using (var fileStream = System.IO.File.OpenRead("/Users/odinndagur/Code/Github/binary_writer_py/test_heights34.dat"))
-            using (var reader = new System.IO.BinaryReader(fileStream))
-            {
-                for(int i = 0; i < width * height; i++){
-                    bigArr[i] = reader.ReadSingle();
-                }
-            }
-        }
-        catch(System.Exception e){ // handle errors here.
-            Debug.Log(e);
-        }
-    }
-    [ContextMenu("Load data with skips")]
-    void loadDataWithSkips(){
-        int data_width = 25000;
-        int data_height = 24999;
-        width = 6000;
-        height = 6000;
-        bigArr = new float[width*height];
-        try
-        {
-            using (var fileStream = System.IO.File.OpenRead("/Users/odinndagur/Code/Github/binary_writer_py/test_heights57.dat"))
-            using (var reader = new System.IO.BinaryReader(fileStream))
-            {
-
-                //testing the skips
-                for(int j = 0; j < zoff; j++){
-                    for(int jj = 0; jj < data_width; jj++){
-                        reader.ReadSingle(); //throw away entire lines until we get to correct zoff line
-                    }
-                    for(int jj = 0; jj < xoff; jj++){
-                        reader.ReadSingle(); //throw away x values until we get to correct one
-                    }
-                }
-                for(int y = zoff, i = 0; y < height + zoff; y++){
-                    for(int x = xoff; x < width + xoff; x++){
-                        bigArr[i++] = reader.ReadSingle();
-                    }
-                    for(int j = xoff; j < data_width - width; j++){
-                        reader.ReadSingle(); //throw away rest of the line and the beginning of next line
-                    }
-                }
 
 
-                // for(int i = 0; i < width * height; i++){
-                //     bigArr[i] = reader.ReadSingle();
-                // }
-            }
-        }
-        catch(System.Exception e){ // handle errors here.
-            Debug.Log(e);
-        }
-    }
-
-    // #endif
     void OnEnable () {
         // loadData();
 		positionsBuffer = new ComputeBuffer((xSize+1)*(zSize+1), 3*sizeof(float));
@@ -211,37 +124,7 @@ public class CoarseMeshGen : MonoBehaviour
             }
         }
     }
-    [ContextMenu("Generate buffers large")]
-    void generateBuffersLarge(){
-        int buffSize = (xSize + 1) * (zSize + 1);
-        buff = new Vector3[buffSize];
-        buff2 = new Vector3[buffSize];
-
-        int step = 20;
-        
-        for (int i = 0, z =0; z<= zSize * step; z+= step)
-        {
-            for (int x = 0; x<=xSize * step; x+= step)
-            {
-                float y = get_height(xoff + x, zoff + z);
-                buff[i] = new Vector3(x, y, z);
-                y = get_height(xoff + xSize + x, zoff + zSize + z);
-                buff2[i] = new Vector3(x, y, z);
-                i++;
-            }
-        }
-
-        // for (int i = 0, z = 0; z<= zSize * step; z+= step) {
-        //     for (int x = 0; x<=xSize * step; x+= step) {
-        //         float y = get_height(x, z);
-        //         buff[i] = new Vector3(x, y, z);
-        //         y = get_height(xSize + x, zSize + z);
-        //         buff2[i] = new Vector3(x, y, z);
-        //         i++;
-        //     }
-        // }
-    }
-
+    
     [ContextMenu("Load custom mesh data and generate buffers")]
     void loadMeshAndGenerateBuffers(){
         int buffSize = (xSize + 1) * (zSize + 1);
@@ -292,30 +175,6 @@ public class CoarseMeshGen : MonoBehaviour
                 buff2[i] = new Vector3(x, temp2[i], z);
                 i++;
             }
-        }
-
-
-
-
-
-
-        // for (int i = 0, z = 0; z<= zSize * step; z+= step) {
-        //     for (int x = 0; x<=xSize * step; x+= step) {
-        //         float y = get_height(x, z);
-        //         buff[i] = new Vector3(x, y, z);
-        //         y = get_height(xSize + x, zSize + z);
-        //         buff2[i] = new Vector3(x, y, z);
-        //         i++;
-        //     }
-        // }
-    }
-    float get_height(int x, int z){
-        float height = bigArr[z * width + x];
-        if(height < -100.0f){
-            return -100.0f;
-        }
-        else {
-            return height;
         }
     }
 
